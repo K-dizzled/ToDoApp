9d469f0a8b0042430ef3238167c77a6662d4edcb
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2021-11-01 17:06:38 +0300

    Start doing the screen with list of to-do-s

--- ToDoApp/TDCell.swift
+++ ToDoApp/TDCell.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+class TDCell : UITableViewCell {
+    @IBOutlet weak var doneCheckbox: UIButton!
+    @IBOutlet weak var importanceMarker: UILabel!
+    
+    @IBOutlet weak var taskStack: UIStackView!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var taskDate: UILabel!
+    
+    @IBOutlet weak var openButton: UIButton!
+    
+    func setTodo(todo: ToDoItem) {
+        taskLabel.text = todo.text
+        taskDate.isHidden = true
+        importanceMarker.isHidden = true
+    }
+}
--- ToDoApp/TdListViewController.swift
+++ ToDoApp/TdListViewController.swift
@@ -8,20 +8,30 @@
 import Foundation
 import UIKit
 
-class TdListViewController : UITableView, UITableViewDelegate, UITableViewDataSource {
+class TdListViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var TDTableView: UITableView!
     
-    
-    var list = FileCache()
+    var list : FileCache = FileCache()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let todo = list.tdList[indexPath.row]
+        let todo = list.toDoList[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "TDCell") as! TDCell
         
         cell.setTodo(todo: todo)
         return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        list.addNewTD(ToDoItem(text: "Сделать", importance: Importance.ordinary))
+        
+        TDTableView.delegate = self
+        TDTableView.dataSource = self
+        
     }
 }
